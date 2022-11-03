7a6f7ba6be9df2fcbbfc6027891e9d3fda70ed73
Ishiyuka <shijing05youjia@gmail.com>
2022-11-03 14:12:41 +0900

    検索ロジックのModel Specを実装

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -21,4 +21,33 @@ RSpec.describe 'タスクモデル機能', type: :model do
       end
     end
   end
+
+  describe '検索機能' do
+    let!(:task) { FactoryBot.create(:task, list: 'タスク1', status:'未着手') }
+    let!(:second_task) { FactoryBot.create(:second_task, list: 'タスク2', status:'着手中')}
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      it "検索キーワードを含むタスクが絞り込まれる" do
+        expect(Task.search_list('タスク1')).to include(task)
+        expect(Task.search_list('タスク1')).not_to include(second_task)
+        expect(Task.search_list('タスク1').count).to eq 1
+      end
+    end
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        expect(Task.search_status('未着手')).to include (task)
+        expect(Task.search_status('未着手')).not_to include (second_task)
+        expect(Task.search_status('未着手').count).to eq 1
+      end
+    end
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスクが絞り込まれる" do
+        expect(Task.search_list('タスク2')).to include(second_task)
+        expect(Task.search_status('着手中')).to include(second_task)
+        expect(Task.search_list('タスク2')).not_to include(task)
+        expect(Task.search_status('着手中')).not_to include(task)
+        expect(Task.search_list('タスク2').count).to eq 1
+        expect(Task.search_status('着手中').count).to eq 1
+      end
+    end
+  end
 end
